71976328bb3c18ff8714f9f4c860edc8c2075851
CS2103Lerulatus <CS2103Lerulatus@protonmail.com>
2022-01-31 06:17:55 +0800

    :sparkles: Level-9-:gift:feature-wrapup

--- src/main/java/cedar/CliParse.java
+++ src/main/java/cedar/CliParse.java
@@ -131,6 +131,30 @@ public class CliParse {
                 }
             }
             break;
+        // todo find findtasks findtask etc. done.
+        case "searchtasks":
+        case "searchtask":
+        case "searchkeyword":
+        case "findtask":
+        case "findtasks":
+        case "search":
+        case "find":
+            String searchPhrase = String.join(" ", Arrays.copyOfRange(command, 1, command.length));
+            ArrayList<Task> matchingPhraseList = Cedar.internalTaskList.stream()
+                    .filter(tasks -> tasks.getDesc().toLowerCase().contains(searchPhrase.toLowerCase()))
+                    .collect(Collectors.toCollection(ArrayList::new));
+            if (matchingPhraseList.size() == 0) {
+                System.out.format("🔎 Search returned 0 hits for \"%s\".\n", searchPhrase);
+            } else {
+                System.out.format("🔎 Search returned %d hit(s).\n", matchingPhraseList.size());
+                //System.out.format("🔎 Search returned %d hit(s) for \"%s\".\n", matchingPhraseList.size(), searchPhrase);
+                System.out.println("Here are your results:");
+                int index = 1;
+                for (Task task : matchingPhraseList) {
+                    System.out.format("%d: " + task.toString() + "\n", index++);
+                }
+            }
+            break;
         default:
             System.out.println("Unrecognized. Check for typo(s) in commands.");
             System.out.println("Type \"help\" for a list of available commands.");
